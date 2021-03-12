a1b60662e7b54cc15a2ff04394e0af71123cffac
frankaging <zxw0424@gmail.com>
2021-03-12 03:34:35 -0800

    init world model

---
+++ code/grammer/vocab.py
@@ -0,0 +1,4 @@
+INTRANSITIVE_VERBS = {"walk"}
+TRANSITIVE_VERBS = {"push", "pull"}
+ADVERBS = {"quickly", "slowly", "while zigzagging", "while spinning", "cautiously", "hesitantly"}
+# Note that different from gSCAN all other languages are generated via grounding first!
