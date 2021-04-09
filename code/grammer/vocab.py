df0ba81029231f8a554a72e913dcebd1d7c31007
frankaging <zxw0424@gmail.com>
2021-04-09 00:47:34 -0700

    update init for second around

--- code/grammer/vocab.py
+++
@@ -1,4 +0,0 @@
-INTRANSITIVE_VERBS = {"walk"}
-TRANSITIVE_VERBS = {"push", "pull"}
-ADVERBS = {"quickly", "slowly", "while zigzagging", "while spinning", "cautiously", "hesitantly"}
-# Note that different from gSCAN all other languages are generated via grounding first!
